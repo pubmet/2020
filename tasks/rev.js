24486f0ccb8039ffcee1636e0cb38ebff3669046
Matija Marohnić <matija.marohnic@gmail.com>
2020-07-07 10:01:23 +0200

    Delete original unrevved assets

--- tasks/rev.js
+++ tasks/rev.js
@@ -1,12 +1,14 @@
 const gulp = require('gulp')
 const rev = require('gulp-rev')
 const revRewrite = require('gulp-rev-rewrite')
+const revDelOriginal = require('gulp-rev-delete-original')
 const { destDir } = require('../etc/build-config')
 
 const revisionAssets = () => {
   return gulp
     .src(`${destDir}/**/*.{css,js}`)
     .pipe(rev())
+    .pipe(revDelOriginal())
     .pipe(gulp.src(`${destDir}/**/*.html`))
     .pipe(revRewrite())
     .pipe(gulp.dest(destDir))
