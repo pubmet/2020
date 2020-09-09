b42f75cc266ca4b25e54279fe6ce9c9f8644d6ff
Matija Marohnić <matija.marohnic@gmail.com>
2020-09-09 21:12:29 +0200

    Bump @pubmet/scripts

--- tasks/publish.js
+++ tasks/publish.js
@@ -2,7 +2,7 @@ const pubmet = require('@pubmet/scripts')
 const { destDir } = require('../etc/build-config')
 
 const publish = async () => {
-  await pubmet.deploy({
+  await pubmet.deployDir({
     localDir: `${destDir}/pubmet2020`,
     remoteDir: 'pubmet2020',
     username: process.env.USERNAME,
